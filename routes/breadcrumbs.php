63a91e1d5506bce2487b4f4a69518c6b23a6f3da
Pedro Augusto <pams.pedro@gmail.com>
2020-01-05 23:43:56 -0300

    improvement: reinstal preset ha-web

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -6,26 +6,30 @@ Breadcrumbs::for('index', function ($breadcrumbs) {
     $breadcrumbs->push(Lang::get('app.index'), route('index'));
 });
 
-Breadcrumbs::for('categories.index', function ($breadcrumbs) {
-   $breadcrumbs->push(Lang::get('categories.index'), route('categories.index'));
+Breadcrumbs::for('home', function ($breadcrumbs) {
+    $breadcrumbs->push(Lang::get('app.home'), route('home'));
 });
 
-Breadcrumbs::for('categories.create', function ($breadcrumbs) {
-   $breadcrumbs->parent('categories.index');
-   $breadcrumbs->push(Lang::get('categories.create'), route('categories.create'));
+Breadcrumbs::for('login', function ($breadcrumbs) {
+    $breadcrumbs->push(Lang::get('login'), route('login'));
 });
 
-Breadcrumbs::for('categories.show', function ($breadcrumbs, $category) {
-   $breadcrumbs->parent('categories.index');
-   $breadcrumbs->push(Lang::get('categories.show', ['category' => $category->id]), route('categories.show', $category->id));
+Breadcrumbs::for('logout', function ($breadcrumbs) {
+    $breadcrumbs->push(Lang::get('logout'), route('logout'));
 });
 
-Breadcrumbs::for('categories.edit', function ($breadcrumbs, $category) {
-   $breadcrumbs->parent('categories.show', $category);
-   $breadcrumbs->push(Lang::get('categories.edit', ['category' => $category->id]), route('categories.edit', $category->id));
+Breadcrumbs::for('register', function ($breadcrumbs) {
+    $breadcrumbs->push(Lang::get('register'), route('register'));
 });
 
-Breadcrumbs::for('categories.delete', function ($breadcrumbs, $category) {
-   $breadcrumbs->parent('categories.show', $category);
-   $breadcrumbs->push(Lang::get('categories.delete', ['category' => $category->id]), route('categories.delete', $category->id));
+Breadcrumbs::for('password.confirm', function ($breadcrumbs) {
+    $breadcrumbs->push(Lang::get('password.confirm'), route('password.confirm'));
+});
+
+Breadcrumbs::for('password.email', function ($breadcrumbs) {
+    $breadcrumbs->push(Lang::get('password.email'), route('password.email'));
+});
+
+Breadcrumbs::for('password.reset', function ($breadcrumbs) {
+    $breadcrumbs->push(Lang::get('password.reset'), route('password.reset'));
 });
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ Route::get('/', 'IndexController@index')->name('index');
 Route::get('/categories/data', 'CategoryController@data')->name('categories.data');
 Route::get('/categories/{category}/delete', 'CategoryController@delete')->name('categories.delete');
 Route::resource('categories', 'CategoryController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'IndexController@index')->name('index');
